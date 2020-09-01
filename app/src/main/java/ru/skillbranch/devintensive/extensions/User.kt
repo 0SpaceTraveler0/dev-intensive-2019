51108e6c28acaaef18b6008b38fc30030414f439
0SpaceTraveler0 <t34ilya2014@mail.ry>
2020-09-01 16:35:54 +0500

    йцу

--- app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.devintensive.extensions
 import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.models.UserView
 import ru.skillbranch.devintensive.utils.Utils
+//проверка
 
 fun User.toUserView(): UserView {
 
